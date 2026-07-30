259085b628cd75b56414b1b54ee87f21d65da039
michael-barchy <michael.barchy@gmail.com>
2026-07-30 22:18:56 +0200

    main

--- storage.mjs
+++ storage.mjs
@@ -1,6 +1,6 @@
 export default [
-    'https://cdn.jsdelivr.net/gh/michael-barchy/koncerto@bfa6f2acb0aaa0201e40dd5e452a82a4e24cc6d3/koncerto.php',
-    'https://cdn.jsdelivr.net/gh/michael-barchy/impulsus@a53e7e1b64c159c71b1977e08f24b75b411d67de/impulsus.js',
+    'https://cdn.jsdelivr.net/gh/michael-barchy/koncerto@main/koncerto.php',
+    'https://cdn.jsdelivr.net/gh/michael-barchy/impulsus@main/impulsus.js',
     'https://cdn.jsdelivr.net/gh/Skrol29/tinybutstrong@master/tbs_class.php',
     'bootstrap.php'
 ];
